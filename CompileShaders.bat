76a50be88944461f1a10a0213d8ae393b8986e78
lowpolypixel <ajay.vikram2110@gmail.com>
2026-03-16 23:57:08 +0530

    Incomplete pipeline implementation

---
+++ CompileShaders.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.4.341.1\Bin\glslc.exe simple_shader.vert -o shaders\simple_shader_vert.spv
+C:\VulkanSDK\1.4.341.1\Bin\glslc.exe simple_shader.frag -o shaders\simple_shader_frag.spv
+pause
