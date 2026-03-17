2ff5274df6c402788dc7d97668b820370b7deea4
lowpolypixel <ajay.vikram2110@gmail.com>
2026-03-17 22:27:38 +0530

    Implement createWindowSurface for Vulkan support
    Added createWindowSurface method to handle Vulkan surface creation.

--- dwe_window.cpp
+++ dwe_window.cpp
@@ -1,5 +1,5 @@
 #include "dwe_window.hpp"
-
+#include <stdexcept>
 namespace dwe {
 	dweWindow::dweWindow(int w, int h, std::string t) : width{ w }, height{ h }, title{ t } {
 		initWindow();
@@ -13,5 +13,10 @@ namespace dwe {
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Disable GLFW's default resizable functions, since we'll handle those differently.
 		window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+	}
+	void dweWindow::createWindowSurface(VkInstance instance, VkSurfaceKHR* surface) {
+		if (glfwCreateWindowSurface(instance, window, nullptr, surface) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create window surface");
+		}
 	}
 }
