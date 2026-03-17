5b746072149881e5d52f9890632768d6626ae069
lowpolypixel <ajay.vikram2110@gmail.com>
2026-03-17 22:26:43 +0530

    Update dwe_pipeline.hpp

--- dwe_pipeline.hpp
+++ dwe_pipeline.hpp
@@ -1,13 +1,26 @@
 #pragma once
+#include "dwe_device.hpp"
 #include <string>
 #include <vector>
 namespace dwe {
 	class dwePipeline {
+		struct pipelineConfigInfo {
+
+		};
 	public:
-		dwePipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+		dwePipeline(dweDevice& device, const std::string& vertFilepath, const std::string& fragFilepath, const pipelineConfigInfo& configInfo);
+		~dwePipeline() {}
+		dwePipeline(const dwePipeline&) = delete;
+		void operator = (const dwePipeline&) = delete;
+		static pipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 	private:
 		static std::vector<char> readFile(const std::string& filepath);
-		void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+		void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath, const pipelineConfigInfo& configInfo);
+		void createShaderModule(const std::vector<char>& code, VkShaderModule*shaderModule);
+		dweDevice& device;
+		VkPipeline graphicsPipeline;
+		VkShaderModule vertShaderModule;
+		VkShaderModule fragShaderModule;
 		
 	};
 }
