9ffb3be0504440f4b7c925dea10aef073a0f966a
lowpolypixel <ajay.vikram2110@gmail.com>
2026-04-01 15:36:07 +0530

    Update dwePipeline configInfo

--- dwe_pipeline.hpp
+++ dwe_pipeline.hpp
@@ -2,12 +2,27 @@
 #include "dwe_device.hpp"
 #include <string>
 #include <vector>
+#include <vulkan/vulkan.h>
+
 namespace dwe {
 	class dwePipeline {
-		struct pipelineConfigInfo {
-
-		};
 	public:
+		struct pipelineConfigInfo {
+			VkPipelineVertexInputStateCreateInfo vertexInputInfo;
+			VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+			VkViewport viewport;
+			VkRect2D scissor;
+			VkPipelineViewportStateCreateInfo viewportInfo;
+			VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+			VkPipelineMultisampleStateCreateInfo multisampleInfo;
+			VkPipelineColorBlendAttachmentState colorBlendAttachment;
+			VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+			VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+			VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
+			VkRenderPass renderPass = VK_NULL_HANDLE;
+			uint32_t subpass = 0;
+		};
+
 		dwePipeline(dweDevice& device, const std::string& vertFilepath, const std::string& fragFilepath, const pipelineConfigInfo& configInfo);
 		~dwePipeline() {}
 		dwePipeline(const dwePipeline&) = delete;
@@ -16,11 +31,10 @@ namespace dwe {
 	private:
 		static std::vector<char> readFile(const std::string& filepath);
 		void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath, const pipelineConfigInfo& configInfo);
-		void createShaderModule(const std::vector<char>& code, VkShaderModule*shaderModule);
+		void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
 		dweDevice& device;
-		VkPipeline graphicsPipeline;
-		VkShaderModule vertShaderModule;
-		VkShaderModule fragShaderModule;
-		
+		VkPipeline graphicsPipeline = VK_NULL_HANDLE;
+		VkShaderModule vertShaderModule = VK_NULL_HANDLE;
+		VkShaderModule fragShaderModule = VK_NULL_HANDLE;
 	};
 }
